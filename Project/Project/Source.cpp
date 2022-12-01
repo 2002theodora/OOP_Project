5679f7cade71c4bb0a115d5375340fc779529d62
AnCSIEtate <2002.theodora@gmail.com>
2022-12-01 21:29:25 +0200

    Made class

--- Project/Project/Source.cpp
+++ Project/Project/Source.cpp
@@ -1,6 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
 using namespace std;
 
+enum type {number, name, asdf};
+
+class table {
+	const string name = nullptr;
+	int noColumns = 0;
+	int size = 0;
+	type type = asdf;
+	 
+};
+
+
+
 int main() {
-	cout << endl << "Hello!";
+	//create table pateu ((nume,name, 12,bucovina),(stoc,number,10,0))
 }
